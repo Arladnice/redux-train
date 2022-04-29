bbb07d3214fb84d2358156b3a369075da202e7b3
Denis Arlakov <darlakov@mail.ru>
2022-04-30 01:54:40 +0300

    basic (2)
    State, reducer, action + redux hooks useDispatch и useSelector

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useDispatch, useSelector } from "react-redux";
+import cn from "./App.module.css";
+
+function App() {
+  const cash = useSelector((state) => state.cash);
+  const dispatch = useDispatch();
+
+  const addCash = (num) => {
+    dispatch({ type: "ADD_CASH", payload: num });
+  };
+
+  const getCash = (num) => {
+    dispatch({ type: "GET_CASH", payload: num });
+  };
+
+  return (
+    <div className={cn.wrapper}>
+      <button onClick={() => addCash(Number(prompt()))} className={cn.button}>
+        Пополнить счет
+      </button>
+      <button onClick={() => getCash(Number(prompt()))} className={cn.button}>
+        Снять со счета
+      </button>
+      <h1 className={cn.cash}>{cash}</h1>
+    </div>
+  );
+}
+
+export default App;
